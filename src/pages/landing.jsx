d9f1958a1c21c17b9d920254784fe8f68c3fff43
aniket <aniketgholap1995@gmail.com>
2025-01-07 23:16:31 +0530

    build issue

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,6 +1,6 @@
 import Hero from 'sections/landing/home/Header';
 import WhatWeOffer from 'sections/landing/home/WhatWeOffer'
-import WhyChooseUs from 'sections/landing/home/WhyChooseUS'
+import WhyChooseUs from 'sections/landing/home/WhyChooseUs'
 import TestSeriesSection from 'sections/landing/home/TestSeries';
 import Courses from 'sections/landing/home/Courses';
 import Testimonial from 'sections/landing/home/Testimonial';
