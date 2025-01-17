70cdf6efc70075d06bea33b8d795bcf0d9e13571
aniket <aniketgholap1995@gmail.com>
2025-01-17 21:21:51 +0530

    empty

--- src/layout/Auth/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-// ==============================|| LAYOUT - AUTH ||============================== //
-
-export default function AuthLayout() {
-  return (
-    <Outlet />
-  );
-}
