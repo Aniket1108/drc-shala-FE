dfe0a2a61abc3c7de0710101158a60b382262164
aniket <aniketgholap1995@gmail.com>
2025-02-07 23:07:58 +0530

    all questions

--- src/pages/management/questions/all_questions.jsx
+++ src/pages/management/questions/all_questions.jsx
@@ -1,32 +1,15 @@
-import React, { useState, useMemo } from 'react';
 import {
-    Paper,
-    TablePagination,
-    Chip,
-    Typography,
-    FormGroup,
-    FormControlLabel,
-    Checkbox,
-    Box,
-    Divider,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    Button,
-    Select,
-    MenuItem,
-    Stack,
-    Collapse,
-    Grid,
-    Card,
-    CardContent,
+    Divider, Paper, TablePagination
 } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 import QuestionsFilter from './all_questions_components/QuestionsFilter';
 import QuestionsStack from './all_questions_components/QuestionsStack';
+import { useHttp } from 'src/utils/api_intercepters.js';
 
 const UserTable = ({ }) => {
+    const useHttpMethod = useHttp();
+
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5);
     const [activeFilters, setActiveFilters] = useState({
@@ -34,6 +17,7 @@ const UserTable = ({ }) => {
         standard: new Set(),
         status: new Set(),
     });
+    const [allQuestions, setAllQuestions] = useState([]);
 
     const handleChangePage = (_event, newPage) => {
         setPage(newPage);
@@ -43,6 +27,18 @@ const UserTable = ({ }) => {
         setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
     };
+
+
+    useEffect(() => {
+        useHttpMethod.get('/admin/questions/list').then(res => {
+            if (res.statusCode == 200) {
+                setAllQuestions(res.data.result)
+            } else {
+                alert(res.message)
+            }
+        });
+
+    }, [])
 
     return (
         <Paper sx={{ width: '100%', overflow: 'hidden' }}>
@@ -55,7 +51,7 @@ const UserTable = ({ }) => {
             <Divider />
 
             <QuestionsStack
-                activeFilters={activeFilters}
+                allQuestions={allQuestions}
             />
 
             <TablePagination
